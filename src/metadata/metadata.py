d4f5a6d1cb3c2c22e2c96a22da76d6913881f9de
alex_g <galamaga.aa@e5ng.ru>
2022-08-07 08:06:40 +0300

    todo

--- src/metadata/metadata.py
+++ src/metadata/metadata.py
@@ -8,3 +8,11 @@
 ## we should modify target storage
 ## and be careful because it might affect other queries to the same data target
 ## and if it is a complete reload we should(?) clear or move outdated data to something like ".old" tables
+
+
+##ToDo
+## overall, the metadata DB contains 3 tables: metadata, scedule and statuses
+## the metadata is to be stored in Postgres DB in a format of jsonb
+## the scedule is for automatical issue of queries
+## the statuses contains id of a query, id for each particular transaction, datetime of it, and a status
+## (for each step i.e. start_get, write_raw, write_file, error, on_hold, canceled, etc.)
