f214bdd6c16fe7d57d2284ab0e8d817668422f73
alex_g <galamaga.aa@e5ng.ru>
2022-08-07 12:08:18 +0300

    comments

--- main.py
+++ main.py
@@ -10,3 +10,22 @@ result = write_data(data,"csv")
 print("Готово!")
 
 
+
+##ToDo
+## UseCase A: Create Query
+## A.1. Ask DB for metadata (column names and types)
+## A.2. Chose columns to get (connect types, etc.)
+## A.3. Save Metadata into metadata DB
+## A.4. Query data from source DB
+## +
+## UseCase B: Correct Query
+## B.1. Ask DB for metadata (column names and types)
+## B.2. Compare new metadata to saved metadata
+## B.3. Update metadata DB
+## +
+## UseCase C: Use Query
+## C.1. Ask DB for metadata (column names and types)
+## C.2. Compare new metadata to saved metadata
+## C.2.5 Ask user to correct the scheme if old!=new, save metadata changes
+## C.3 Query data from source DB
+
